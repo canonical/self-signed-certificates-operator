742c630f2c97bdf00d4f4c588c2175d33b16f46d
Bartlomiej Gmerek <42570669+Gmerold@users.noreply.github.com>
2025-03-04 16:53:23 +0100

    fix: Adjust the TF module to CC006 (#308)

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,13 +3,16 @@
 
 resource "juju_application" "self-signed-certificates" {
   name  = var.app_name
-  model = var.model_name
+  model = var.model
 
   charm {
-    name    = "self-signed-certificates"
-    channel = var.channel
-    base    = "ubuntu@24.04"
+    name     = "self-signed-certificates"
+    channel  = var.channel
+    revision = var.revision
+    base     = var.base
   }
-  config = var.config
-  units  = 1
+
+  config      = var.config
+  constraints = var.constraints
+  units       = var.units
 }
