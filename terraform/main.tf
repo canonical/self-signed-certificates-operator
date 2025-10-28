8ffb680fb2c73905f03734e3cfc9e43cf6c9bd3a
Michael Thamm <mike.thamm@canonical.com>
2025-10-28 12:39:15 -0400

    chore: pin TF Juju provider to `version = ">= 1.0.0"`

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,8 @@
 # See LICENSE file for licensing details.
 
 resource "juju_application" "self-signed-certificates" {
-  name  = var.app_name
-  model = var.model
+  name       = var.app_name
+  model_uuid = var.model_uuid
 
   charm {
     name     = "self-signed-certificates"
@@ -19,14 +19,14 @@ resource "juju_application" "self-signed-certificates" {
 
 resource "juju_offer" "send_ca_cert" {
   name             = "send-ca-cert"
-  model            = var.model
+  model_uuid       = var.model_uuid
   application_name = var.app_name
   endpoints        = ["send-ca-cert"]
 }
 
 resource "juju_offer" "certificates" {
   name             = "certificates"
-  model            = var.model
+  model_uuid       = var.model_uuid
   application_name = var.app_name
   endpoints        = ["certificates"]
 }
